15c7f574d39b0143f5cb9c7436d015f1c63b2594
Dilip Kumar <dilipkp123@gmail.com>
2024-12-29 18:53:48 +0530

    feature: handle test for string consist delimiter

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -10,4 +10,16 @@ describe("String calculator", () => {
   test("should return sum of commas separated string", () => {
     expect(StringCalculator("1,2,3")).toBe(6);
   });
+  test("should return 0 for empty input with delimiter", () => {
+    expect(StringCalculator("//;\n")).toBe(0);
+  });
+  test("should return the sum of numbers separated by newlines", () => {
+    expect(StringCalculator("4\n5\n6")).toBe(15);
+  });
+  test("should return the single number with delimiter", () => {
+    expect(StringCalculator("//;\n1")).toBe(1);
+  });
+  test("should handle delimiter string with sum of number;", () => {
+    expect(StringCalculator("//;\n1;2,3\n4")).toBe(10);
+  });
 });
