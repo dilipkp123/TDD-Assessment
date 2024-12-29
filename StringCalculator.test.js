3158e63fe8229532dcba37b925c72b45d5b61c2d
Dilip Kumar <dilipkp123@gmail.com>
2024-12-29 18:44:41 +0530

    feature: Add test case for singe input and commas separated string

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -4,4 +4,10 @@ describe("String calculator", () => {
   test("should return 0 if string is empty", () => {
     expect(StringCalculator("")).toBe(0);
   });
+  test("should return the number itself for single input", () => {
+    expect(StringCalculator("5")).toBe(5);
+  });
+  test("should return sum of commas separated string", () => {
+    expect(StringCalculator("1,2,3")).toBe(6);
+  });
 });
